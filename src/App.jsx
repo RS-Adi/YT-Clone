adf237d72c51581fd472a662ae310ee4ca52e650
RS-Adi <srepudi@uwm.edu>
2025-02-25 14:50:36 -0800

    Every component shoudl be in uppercase only Renamed all components, //done for the day//

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/navbar/navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
     </div>
   )
 }
