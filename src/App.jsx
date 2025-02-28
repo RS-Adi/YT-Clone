0e2c9745c7774b6f074dc3cd751e6ab298179f33
RS-Adi <srepudi@uwm.edu>
2025-02-27 16:30:43 -0800

    Trying to add home to the app but something is going wrong

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
+        <Routes>
+        </Routes>
       
     </div>
   )
