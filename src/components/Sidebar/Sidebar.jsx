dce385715f2ee2bdde890abdab6efc17bb37a690
RS-Adi <srepudi@uwm.edu>
2025-03-11 17:32:23 -0700

    Creatyed home page and added feed to the page inserted thumbnails and now going to beautify them

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -15,9 +15,9 @@ import tom from '../../assets/tom.png'
 import megan from '../../assets/megan.png'
 import cameron from '../../assets/cameron.png'
 
-const Sidebar = () => {
+const Sidebar = ({sidebar}) => {
   return (
-    <div className= 'sidebar'>
+    <div className= {`sidebar ${sidebar?"":"small-sidebar"}`}>
         <div className="shortcut-links">
           <div className="side-links">
             <img src={home} alt="" /><p>Home</p>
@@ -62,7 +62,6 @@ const Sidebar = () => {
           <div className="side-link">
             <img src={tom} alt="" /> <p>Sub1</p>
           </div>
-          
           <div className="side-link">
             <img src={cameron} alt="" /> <p>Sub2</p>
           </div>
